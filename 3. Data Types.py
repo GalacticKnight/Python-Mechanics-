c47180ff4c176de8fa2ec9547f66814ef0151b41
GalacticKnight <35178680+GalacticKnight@users.noreply.github.com>
2021-02-01 15:27:30 -0500

    Create 3. Data Types.py

---
+++ 3. Data Types.py
@@ -0,0 +1,117 @@
+#String
+x = str("Hello World")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Integer
+x = int(20)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Float
+x = float(20.5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+#Complex
+x = complex(1j)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+#List
+x = list(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Tuple
+x = tuple(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Range
+x = range(6)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#dictionary
+x = dict(name="John", age=36)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#set
+x = set(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Frozenset
+x = frozenset(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Bool
+x = bool(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#Byte
+x = b"Hello"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#bytearray
+x = bytearray(5)
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+#memoryview
+x = memoryview(bytes(5))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+'''
+x = "Hello World"	                            str	
+x = 20	                                      int	
+x = 20.5	                                    float	
+x = 1j	                                      complex	
+x = ["apple", "banana", "cherry"]	            list	
+x = ("apple", "banana", "cherry")	            tuple	
+x = range(6)	                                range	
+x = {"name" : "John", "age" : 36}	            dict	
+x = {"apple", "banana", "cherry"}	            set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+x = True	                                    bool	
+x = b"Hello"	                                bytes	
+x = bytearray(5)	                            bytearray	
+x = memoryview(bytes(5))	                    memoryview
+'''
